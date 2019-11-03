3c9f76d3a121fccc38b450495bef298236cee952
Reihan35 <hamissi20@yahoo.com>
2019-11-03 19:01:24 +0100

    clean up

--- aagaTME3/scripts/sctipt2.py
+++ aagaTME3/scripts/sctipt2.py
@@ -1,17 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt('temps.txt')
+data = np.loadtxt('tailleBD2MSteiner.txt')
 i = 0
 for column in data.T:
   if i == 1:
-  	plt.plot(data[:,0], column, label = "Base de Test 2 avec méthode 1")
+  	plt.plot(data[:,0], column, label = "Base de Test 2 avec méthode 2")
   if i == 2 : 
   	plt.plot(data[:,2],column,label = "Base de Test 1 avec méthode 2")
   i = i + 1
 
 plt.xlabel("Nombre de point du graphe")
-plt.ylabel("Temps d'exécution en milliseconde")
+plt.ylabel("Taille de EDC (en nombre de points)")
 plt.legend()
 plt.show()
 
